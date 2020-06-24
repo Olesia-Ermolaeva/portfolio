e71b5a56f5989940d3fff142bd9cf49f6deb9bc6
Виктор Малышев <ovmalisheva@gmail.com>
2020-06-24 23:02:29 +0300

    feat: add burger-menu for mobile version

--- js/main.js
+++ js/main.js
@@ -264,5 +264,22 @@ $("body").on('click', '[href*="#"]', function(e){
   e.preventDefault();
 });
 
+// бургер-меню
+(function($) {
+  $('.js-nav-menu-toggle').on('click', function() {
+    $(this).parents('.navigation-menu').toggleClass('navigation-menu--open');
+  });
+  
+  $('html').on('click', function(e) {
+    if(!$(e.target).closest('.js-nav-menu').length &&
+      ($('.js-nav-menu').hasClass('navigation-menu--open'))) {
+        $('.js-nav-menu').removeClass('navigation-menu--open');
+    }
+  });
+})(jQuery);
 
+;(function() {
+  // Initialize
+  var bLazy = new Blazy();
+})();
 
