d445f7a6d0b0d88dbe58bfce632b1f883591fdf2
Виктор Малышев <ovmalisheva@gmail.com>
2020-06-28 19:06:10 +0300

    feat: add section review

--- js/main.js
+++ js/main.js
@@ -16,7 +16,6 @@ $(document).ready(function () {
   });
 
 
-  new WOW().init();
 
   //плавная прокрутка вверх
   $(window).scroll(function () {
@@ -193,6 +192,20 @@ window.onscroll = function() {
   // Initialize
   var bLazy = new Blazy();
 })();
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
 
 
